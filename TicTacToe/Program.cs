e649f43554f6c806fd1e17bc50cc0705f4377ab1
ruslanrust <ruslanrust23@gmail.com>
2024-08-04 20:35:10 +0400

    Some fixes

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,28 +5,33 @@ namespace TicTacToe
 {
   internal class Program
   {
-    
+    const char crossSign = 'X';
+    const char roundSign = 'O';
+
     static void Main(string[] args)
     {
       Console.Write("Хотите сыграть против другого игрока? [y/n] ");
       bool startGame = Console.ReadLine() == "y";
-      
+
       if (startGame)
       {
         char[] field = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        char currentPlayer = 'X';
+        char currentPlayer = crossSign;
+
         DrawField(field);        
 
         while (true)
         {                 
           Console.Write($"Ход {currentPlayer}: ");          
           ConsoleKeyInfo cki = Console.ReadKey(true);
-          bool correctInput = int.TryParse(cki.KeyChar.ToString(), out int choise) && choise > 0 && choise <= 9 && field[choise - 1] != 'X' && field[choise - 1] != 'O';        
+          bool correctInput = int.TryParse(cki.KeyChar.ToString(), out int choise) && 
+            choise > 0 && choise <= 9 && field[choise - 1] != crossSign && field[choise - 1] != roundSign;
 
           if (correctInput)
           {
             Console.WriteLine(cki.KeyChar);
             field[choise - 1] = currentPlayer;
+
             DrawField(field);
 
             if (CheckForWin(field))
@@ -41,7 +46,7 @@ namespace TicTacToe
               break;
             }
 
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            currentPlayer = (currentPlayer == crossSign) ? roundSign : crossSign; // смена игрока.
           }
           else
           {
@@ -56,58 +61,59 @@ namespace TicTacToe
       }     
     }
 
+    /// <summary>
+    /// Отрисовываем текущее состояния игрового поля.
+    /// </summary>
+    /// <param name="field">Игровое поле.</param>
     static void DrawField(char[] field)
     {
+      for (int i = 0; i < 9; i += 3)
+      {
+        Console.WriteLine("-------------");
+        Console.Write("| ");
+        DrawSign(field[i]);
+        Console.Write("| ");
+        DrawSign(field[i + 1]);
+        Console.Write("| ");
+        DrawSign(field[i + 2]);
+        Console.WriteLine("|");
+      }
+
       Console.WriteLine("-------------");
-      Console.Write("| ");
-      DrawChar(field[0]);
-      Console.Write("| ");
-      DrawChar(field[1]);
-      Console.Write("| ");
-      DrawChar(field[2]);
-      Console.WriteLine("|");
-      Console.WriteLine("-------------");
-      Console.Write("| ");
-      DrawChar(field[3]);
-      Console.Write("| ");
-      DrawChar(field[4]);
-      Console.Write("| ");
-      DrawChar(field[5]);
-      Console.WriteLine("|");
-      Console.WriteLine("-------------");
-      Console.Write("| ");
-      DrawChar(field[6]);
-      Console.Write("| ");
-      DrawChar(field[7]);
-      Console.Write("| ");
-      DrawChar(field[8]);
-      Console.WriteLine("|");
-      Console.WriteLine();
+      Console.WriteLine();    
     }
 
-    static void DrawChar(char c)
+    /// <summary>
+    /// Отрисовываем знак в игровом поле
+    /// </summary>
+    /// <param name="sign">Знак игрока.</param>
+    static void DrawSign(char sign)
     {
-      if (c == 'X')
+      if (sign == crossSign)
       {
         Console.BackgroundColor = ConsoleColor.Blue;
-        Console.Write($"{c}");
+        Console.Write($"{sign}");
         Console.ResetColor();
         Console.Write(" ");
       }
-
-      else if (c == 'O')
+      else if (sign == roundSign)
       {
         Console.BackgroundColor = ConsoleColor.Red;
-        Console.Write($"{c}");
+        Console.Write($"{sign}");
         Console.ResetColor();
         Console.Write(" ");
       }
       else
       {
-        Console.Write($"{c} ");
+        Console.Write($"{sign} ");
       }
     }
 
+    /// <summary>
+    /// Проверяем на выигрыш
+    /// </summary>
+    /// <param name="field">Игровое поле.</param>
+    /// <returns>True, если есть выигрышная комбинация, иначе - false.</returns>
     static bool CheckForWin(char[] field)
     {
       return (field[0] == field[1] && field[1] == field[2]) ||
@@ -120,11 +126,16 @@ namespace TicTacToe
         (field[2] == field[5] && field[5] == field[8]);
     }
 
+    /// <summary>
+    /// Проверяем на ничью
+    /// </summary>
+    /// <param name="field">Игровое поле.</param>
+    /// <returns>True, если ничья, иначе - false.</returns>
     static bool CheckForDraw(char[] field)
     {
       foreach (char c in field)
       {
-        if (c != 'X' && c != 'O')
+        if (c != crossSign && c != roundSign)
           return false;
       }
 
